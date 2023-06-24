268474892599d888c8f699cc6effd3ae86b9f56a
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-06-24 12:31:16 +0300

    add tailwind

--- copy-paste-components/src/App.vue
+++ copy-paste-components/src/App.vue
@@ -1,33 +1,6 @@
 
 <template>
-  <h1>hi!</h1>
+  <h1 class="text-red-500 font-bold text-2xl">Hi! this place will be better</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
